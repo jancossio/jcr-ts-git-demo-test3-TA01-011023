791ecf2073aef53fef64f208bba7998d2cfa5198
jancossio <jancossio@gmail.com>
2023-10-01 21:53:21 +0200

    Ejercicio 1 TA01-3

---
+++ Test3.java
@@ -0,0 +1,9 @@
+public class ComentarioVariasLineas{
+    public static void main(String[] args){
+        /*System.out.println("Adios!");
+        System.out.println("FIN DE PROGRAMA");*/
+        for(int i=0; i<6; i++)
+            System.out.println(i+" ");
+        System.out.println("FIN DE PROGRAMA");
+    }
+}
